6adbf093cd91a256cb8f201ecfc06bb7dfc5b74c
Tolga Paksoy <tlgpksy@gmail.com>
2022-03-14 13:40:33 +0100

    Revert "Remove requirement of disabling  of NestJS"
    This reverts commit 8fbc8121c0ddfdfc6c98457f477e47d0c6d1884b.
    
    Apparently, converting the parsed webhook JSON back to JSON produces differences in the raw webhook body.
    For instance:
    
    * a `/` is escaped by Shopify, but not escaped by `JSON.stringify`.
    * monetary values are always suffixed with `.0`, but not when you call `JSON.stringify`
    
    this was causing the HMAC signing to fail. In other words, you *need* to disable the bodyParser
    logic of NestJS in order to access the raw webhook body to calculate the HMAC.

--- src/shopify-webhook/shopify-webhook.controller.ts
+++ src/shopify-webhook/shopify-webhook.controller.ts
@@ -1,70 +1,10 @@
-// Based on work at Shopify: https://github.com/Shopify/shopify-node-api/blob/9a3f59d5cd51b50ddad093741dfbaab1f32a6d93/src/webhooks/registry.ts
-// Modified for usage in NestJS.
-
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  ForbiddenException,
-  Headers,
-  HttpCode,
-  Post,
-} from '@nestjs/common';
-import Shopify, { ShopifyHeader } from '@shopify/shopify-api';
-import { createHmac } from 'crypto';
-import safeCompare from './safe-compare.utils';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import Shopify from '@shopify/shopify-api';
 
 @Controller('shopify/webhooks')
 export class ShopifyWebhookController {
   @Post()
-  @HttpCode(200)
-  async handle(
-    @Headers(ShopifyHeader.Hmac) hmac: string,
-    @Headers(ShopifyHeader.Topic) topic: string,
-    @Headers(ShopifyHeader.Domain) domain: string,
-    @Body() body: any,
-  ) {
-    if (!body) {
-      throw new BadRequestException(
-        'No body was received when processing webhook',
-      );
-    }
-
-    const missingHeaders = [];
-    if (!hmac) {
-      missingHeaders.push(ShopifyHeader.Hmac);
-    }
-    if (!topic) {
-      missingHeaders.push(ShopifyHeader.Topic);
-    }
-    if (!domain) {
-      missingHeaders.push(ShopifyHeader.Domain);
-    }
-    if (missingHeaders.length) {
-      throw new BadRequestException(
-        `Missing on or more of the required HTTP headers to process webhooks: [${missingHeaders.join(
-          ', ',
-        )}]`,
-      );
-    }
-
-    const generatedHash = createHmac('sha256', Shopify.Context.API_SECRET_KEY)
-      .update(JSON.stringify(body), 'utf8')
-      .digest('base64');
-
-    if (!safeCompare(generatedHash, hmac)) {
-      throw new ForbiddenException(
-        `Could not validate request for topic ${topic}. Invalid HMAC.`,
-      );
-    }
-
-    const graphqlTopic = (topic as string).toUpperCase().replace(/\//g, '_');
-    const webhookEntry = Shopify.Webhooks.Registry.getHandler(graphqlTopic);
-
-    if (!webhookEntry) {
-      throw new ForbiddenException(`No webhook is registered for ${topic}`);
-    }
-
-    await webhookEntry.webhookHandler(graphqlTopic, domain as string, body);
+  async handle(@Req() req, @Res() res) {
+    await Shopify.Webhooks.Registry.process(req, res);
   }
 }
